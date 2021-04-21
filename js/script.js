c537eaa12c5ed4f179e71f19f528b154522729f9
dani2345 <>
2021-04-21 16:38:14 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("botaoenviar").addEventListener("click",validaFormulario  )
+
+function validaFormulario(){if(document.getElementById("nome").value != "" && document.getElementById("email").value != ""){
+  alert("Prontinho! você receberá as novidades por email.")
+}else}
+  alert(peça a usuária que preencha os campos nome e email")
+  }
+}
+
